e261dc12801638dac8a45d8e3247990b516a43e7
GabrielVL <gavalo.99@gmail.com>
2022-03-10 19:28:43 -0600

    [Add] Beta get Trabajador

---
+++ TABAS_API/TABAS_API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TABAS_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "GetTrabajador")]
+        public string Get(string nombre, string primer_apellido, string segundo_apellido, int cedula, string rol)
+        {
+            return "Nombre: " + nombre + " " + primer_apellido + " " + segundo_apellido + "\n" +
+                "Cédula: " + cedula.ToString() + "\n" +
+                "Rol: " + rol;
+        }
+    }
+}
