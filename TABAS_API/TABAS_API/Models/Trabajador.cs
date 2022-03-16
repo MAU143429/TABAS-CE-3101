a9d953835a2d17c87217474ec9232d46afbacc4a
GabrielVL <gavalo.99@gmail.com>
2022-03-15 22:16:03 -0600

    [Add] Class Trabajador attributes

--- TABAS_API/TABAS_API/Models/Trabajador.cs
+++ TABAS_API/TABAS_API/Models/Trabajador.cs
@@ -2,5 +2,22 @@
 {
     public class Trabajador
     {
+        private string nombre { get; set; }
+        private string apellidos { get; set; }
+        private int cedula { get; set; }
+        private string rol { get; set; }
+
+        public Trabajador()
+        {
+        }
+
+        public Trabajador(string nombre, string apellidos, int cedula, string rol)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.cedula = cedula;
+            this.rol = rol;
+        }
+
     }
 }
