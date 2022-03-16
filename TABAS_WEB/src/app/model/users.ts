3291ec2594573e8cc8a77a5c25d2ae999ea182db
Yendry <yen.badilla@gmail.com>
2022-03-16 00:55:13 -0600

    Init Web-API connection

---
+++ TABAS_WEB/src/app/model/users.ts
@@ -0,0 +1,8 @@
+export class Users {
+    id:number = 0;
+    nombre:string = '';
+    apellidos:string = '';
+    cedula:number = 0;
+    rol:string = '';
+}
+
--- TABAS_WEB/src/app/register/register.component.ts
+++ TABAS_WEB/src/app/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  
   constructor() { }
 
   ngOnInit(): void {
