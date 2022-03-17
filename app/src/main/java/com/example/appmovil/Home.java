649468b25aa420aef268acb8f2d2645427fb6b8e
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2022-03-17 10:35:08 -0600

    [Add] Home

--- app/src/main/java/com/example/appmovil/Home.java
+++ app/src/main/java/com/example/appmovil/Home.java
@@ -2,7 +2,11 @@ package com.example.appmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
