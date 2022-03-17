e9d98782c0c998c1edcc3f9ff0ab88b3a1649779
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2022-03-17 09:39:16 -0600

    [Add] Login - new version

---
+++ app/src/main/java/com/example/appmovil/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.appmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText username = (EditText) findViewById(R.id.user);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("test")) {
+                    login();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    public void login() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        finish();
+    }
+}
