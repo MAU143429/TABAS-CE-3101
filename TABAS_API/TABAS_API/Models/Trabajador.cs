1c1f77b44aa4ffa034b7049cafb3de96c33fba0b
GabrielVL <gavalo.99@gmail.com>
2022-03-17 13:22:45 -0600

    [Add] Correo y telefono attributes in Trabajador

--- TABAS_API/TABAS_API/Models/Trabajador.cs
+++ TABAS_API/TABAS_API/Models/Trabajador.cs
@@ -5,15 +5,19 @@
         public int id { get; set; }
         public string nombre { get; set; }
         public string apellidos { get; set; }
-        public int cedula { get; set; }
+        public long cedula { get; set; }
+        public string correo { get; set; }
+        public long telefono { get; set; }
         public string rol { get; set; }
 
-        public Trabajador(int id, string nombre, string apellidos, int cedula, string rol)
+        public Trabajador(int id, string nombre, string apellidos, long cedula, string correo, long telefono, string rol)
         {
             this.id = id;
             this.nombre = nombre;
             this.apellidos = apellidos;
             this.cedula = cedula;
+            this.correo = correo;
+            this.telefono = telefono;
             this.rol = rol;
         }
 
